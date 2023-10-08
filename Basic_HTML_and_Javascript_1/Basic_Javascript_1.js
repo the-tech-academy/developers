e1db570470409bd38957f784c871cfc638e0470e
AnnieGlade4327 <andreaglade4327@gmail.com>
2023-10-08 13:11:52 -0600

    fixed function declaration error null msg. Bracket was misplaced

--- Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -1,4 +1,8 @@
-var String = "Set your Itinterary!";
-		var result = String.fontcolor("purple");
-		document.getElementById("Ireland").innerHTML = 
-		result
+
+	var String = "Set your Itinerary!";
+	var result = String.fontcolor("purple");
+	document.getElementById("Ireland").innerHTML = result;
+	
+	var String = "Choose Your Meal Plan";
+	var result = String.hexcolor("#FFA07A");
+	document.getElementById("MealPlan").innerHTML = result;
